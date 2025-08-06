4f26ebbdc533cd071bb88df5f6997794656e3679
Valerii Rublev <vedius.rublev@gmail.com>
2025-08-06 20:13:25 +0300

    proverka

--- .history/common/src/main/java/com/viancis/common/exception/InvalidCredentialsException_20241210114720.java
+++
@@ -1,7 +0,0 @@
-package com.viancis.common_point_user.exception;
-
-public class InvalidCredentialsException extends RuntimeException {
-    public InvalidCredentialsException() {
-        super("Invalid username or password");
-    }
-}
--- .history/common/src/main/java/com/viancis/common/exception/UserNotFoundException_20241210114712.java
+++
@@ -1,7 +0,0 @@
-package com.viancis.common_point_user.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String id) {
-        super("User not found with id: " + id);
-    }
-}
